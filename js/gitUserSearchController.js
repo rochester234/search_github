b307651ef846b1486d627e6494d7b8da56047315
Hamza Sheikh <hsheikhm@gmail.com>
2015-12-01 16:46:15 +0000

    just completed auto-search

--- js/gitUserSearchController.js
+++ js/gitUserSearchController.js
@@ -1,24 +1,17 @@
-githubUserSearch.controller('GitUserSearchController', [function() {
+githubUserSearch.controller('GitUserSearchController', ['$resource', function($resource) {
+
+  var searchResource = $resource('https://api.github.com/search/users');
 
   var self = this;
 
-self.searchResult = undefined;
+  self.searchResult = undefined;
 
-self.doSearch = function() {
-  self.searchResult = {
-    items: [
-      {
-        "login": "tansaku",
-        "avatar_url": "https://avatars.githubusercontent.com/u/30216?v=3",
-        "html_url": "https://github.com/tansaku"
-      },
-      {
-        "login": "stephenlloyd",
-        "avatar_url": "https://avatars.githubusercontent.com/u/196474?v=3",
-        "html_url": "https://github.com/stephenlloyd"
-      }
-    ]
+  self.doSearch = function() {
+    self.searchResult = searchResource.get(
+      {q: self.searchTerm},
+      {access_token: '284c888df3ca4478d1840055d8d7f3cc091902bf'}
+    );
+    console.log(self.searchResult);
   };
-};
 
 }]);
